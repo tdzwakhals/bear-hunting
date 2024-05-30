eb2f7f1aca2682ee3feb8b93ac9506b17e8512f1
Tom Zwakhals <tomzwakhals@gmail.com>
2024-05-30 16:28:03 +0200

    Refactor of class name and user operations added

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -7,11 +7,18 @@ namespace App\Service;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use DateTime;
+use Random\RandomException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final readonly class UserService
 {
-    public function __construct(private UserRepository $userRepository)
-    {
+    public function __construct(
+        private UserRepository $userRepository,
+        private ValidatorInterface $validator,
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
     }
 
     public function findOneByIdentifier(string $identifier): ?User
@@ -23,5 +30,50 @@ final readonly class UserService
     {
         $user->setLastLogin(new DateTime());
         $this->userRepository->save($user);
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getAll(): array
+    {
+        return $this->userRepository->findAll();
+    }
+
+    /**
+     * @throws RandomException
+     */
+    public function create(User $user): string
+    {
+        $validationList = $this->validator->validate($user);
+        if ($validationList->count() !== 0) {
+            throw new ValidationFailedException($user, $validationList);
+        }
+
+        $plainPassword = $this->generatePassword();
+
+        $user->setRoles(['ROLE_HUNTER'])
+            ->setPassword(
+                $this->passwordHasher->hashPassword($user, $plainPassword)
+            );
+
+        $this->userRepository->save($user);
+
+        return $plainPassword;
+    }
+
+    /**
+     * @throws RandomException
+     */
+    private function generatePassword(): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 10; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
     }
 }
